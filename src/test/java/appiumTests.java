1bd725f1f958533e370f3c7fba02948473e839ee
Valiantsin Dorum <valiantsin_dorum@epam.com>
2020-01-22 11:48:11 +0300

    first commit

---
+++ src/test/java/appiumTests.java
@@ -0,0 +1,78 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class appiumTests {
+
+    private static final String APP_ANDROID = "";
+    private static final String APP_IOS = "";
+    private static final String APPIUM = "http://localhost:4723/wd/hub";
+
+    AppiumDriver driver;
+
+
+    private void setUpAndroid() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "10");
+        caps.setCapability("automationName", "Appium");
+        caps.setCapability("appPackage", "com.restar.scanner");
+        caps.setCapability("appActivity", "host.exp.exponent.MainActivity");
+        caps.setCapability("appWaitForLaunch", "false");
+
+        //caps.setCapability("app", "C:\\Users\\Valiantsin_Dorum\\Downloads\\app-debug.apk");
+        //caps.setCapability("app", APP_ANDROID);
+        driver = new AndroidDriver(new URL(APPIUM), caps);
+    }
+
+    private void setUpIOS() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("platformVersion", "13.0");
+        caps.setCapability("deviceName", "iPhone X");
+        //caps.setCapability("app", APP_IOS);
+        driver = new IOSDriver(new URL(APPIUM), caps);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        setUpAndroid();
+        //       setUpIOS();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void testLoginPage() throws InterruptedException {
+//        WebDriverWait wait = new WebDriverWait(driver, 90);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-email")));
+
+        driver.findElementByAccessibilityId("login-email").sendKeys("dalekaya.taiga@gmail.com");
+        driver.findElementByAccessibilityId("login-password").sendKeys("FFFtDC1");
+        driver.findElementByAccessibilityId("login-button").click();
+
+        WebElement errorMessage = (WebElement) driver.findElementsByAccessibilityId("login failed");
+        assert errorMessage.getText().equals("login failed");
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
